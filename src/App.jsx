23815c81c83a6ed6c75cfa03fc8175799e431fe9
Marina Perin <maryfrancisco@live.com>
2023-12-07 17:16:32 +0100

    feat | exercise 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,82 +3,64 @@ import "./App.css";
 import CustomList from "./components/CustomList";
 
 function App() {
-  const allLists = [
+  const [allLists, setAllLists] = useState([
     {
       title: "Guests",
       type: "ul",
       list: ["Tizio", "Caio"],
+      value: "",
     },
     {
       title: "Pets",
       type: "ul",
       list: ["Denki", "Yeon", "Jin"],
+      value: "",
     },
     {
       title: "To Do",
       type: "ol",
       list: ["Groceries", "Laundry", "Dishes"],
-    },
-  ];
-  const [list, setList] = useState([
-    {
-      title: "Guests",
-      type: "ul",
-      list: ["Tizio", "Caio"],
-    },
-    {
-      title: "Pets",
-      type: "ul",
-      list: ["Denki", "Yeon", "Jin"],
-    },
-    {
-      title: "To Do",
-      type: "ol",
-      list: ["Groceries", "Laundry", "Dishes"],
+      value: "",
     },
   ]);
-  const [inputValue, setInputValue] = useState("");
 
   return (
-    <>
-      <main>
-        <div>
-          <>
-          {list.map(({ title }) => (
-            <>
-            <h2 key={title}>{title}</h2>
-            <div>
-                <input
-                  type="text"
-                  value={inputValue}
-                  onChange={(e) => 
-                     setInputValue(e.target.value)}
-                />
-                <span>
-                  <button
-                    onClick={() =>
-                      setList([
-                        {
-                          ...l,
-                          inputValue,
-                        }
-                      ])}
-                  >
-                    Add
-                  </button>
-                </span>
-              </div>
-              </>))}
-            {list.map(({ type, list }, i) => (
-            <div key={`list${i}`}>
-              <CustomList type={type} list={list} />
+    <main>
+      {allLists.map((obj, i) => {
+        const { title, type, list, value } = obj;
+        return (
+          <div key={`obj${i}`}>
+            <h2>{title}</h2>
+            <div key={`objInputWrapper${i}`}>
+              <input
+                key={`input${i}`}
+                type="text"
+                value={value}
+                onChange={(e) => {
+                  obj.value = e.target.value;
+                  setAllLists([...allLists]);
+                }}
+              />
+              <span key={`objSpan${i}`}>
+                <button
+                  key={`btn${i}`}
+                  onClick={() => {
+                    obj.list = [...list, value];
+                    setAllLists([...allLists]);
+                    obj.value = '';
+                  }}
+                >
+                  Add
+                </button>
+              </span>
             </div>
-          ))}
-          
-          </>
-        </div>
-      </main>
-    </>
+            <div key={`objListWrapper${i}`}>
+              <CustomList key={`list${i}`} type={type} list={list} />
+            </div>
+          </div>
+        );
+      })}
+    </main>
   );
 }
 
